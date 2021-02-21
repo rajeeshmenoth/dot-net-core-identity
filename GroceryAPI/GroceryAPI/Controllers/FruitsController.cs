ea2ac73643a0aa6ad12c04b1370e4975d4900484
rajeeshmenoth <rajeeshmenoth@gmail.com>
2021-02-21 17:12:07 +0530

    new controller added.

---
+++ GroceryAPI/GroceryAPI/Controllers/FruitsController.cs
@@ -0,0 +1,36 @@
+﻿using GroceryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FruitsController : Controller
+    {
+        IList<Fruits> frutis = new List<Fruits> {
+        new Fruits() {
+            Id = 1,
+            Name = "Apple"
+        },
+        new Fruits(){
+            Id = 2,
+            Name = "Orange"
+        },
+        new Fruits(){
+            Id = 2,
+            Name = "Grape"
+        }
+        };
+
+        [HttpGet]
+        public ActionResult GetFruits()
+        {
+            return Ok(this.frutis);
+        }
+    }
+}
